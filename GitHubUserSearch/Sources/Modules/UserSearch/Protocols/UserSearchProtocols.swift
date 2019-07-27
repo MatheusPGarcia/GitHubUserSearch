a1e2ea969e2251ba67d118a46bfdc8bf9de87373
Matheus <Garcia>
2019-07-26 21:00:49 -0300

    Added userSearch protocols

---
+++ GitHubUserSearch/Sources/Modules/UserSearch/Protocols/UserSearchProtocols.swift
@@ -0,0 +1,19 @@
+//
+//  UserSearchProtocols.swift
+//  GitHubUserSearch
+//
+//  Created by Matheus Garcia on 26/07/19.
+//  Copyright © 2019 Matheus Garcia. All rights reserved.
+//
+
+import Foundation
+
+protocol UserSearchViewControllerToPresenterProtocol: class { }
+
+protocol UserSearchPresenterToViewControllerProtocol: class { }
+
+protocol UserSearchPresenterToInteractorProtocol: class { }
+
+protocol UserSearchInteractorToPresenterProtocol: class { }
+
+protocol UserSearchApiDataManagerProtocol: class { }
